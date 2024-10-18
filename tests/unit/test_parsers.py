4e7e9736d0396a8e2ff5fd8942c832357417d471
samwaseda <o.waseda@mpie.de>
2024-10-18 14:54:39 +0000

    add tests

--- tests/unit/test_parsers.py
+++ tests/unit/test_parsers.py
@@ -4,12 +4,17 @@ from uniton.converter import parse_input_args, parse_output_args
 
 
 class TestUnits(unittest.TestCase):
-    def test_use_list(self):
+    def test_content(self):
         def get_speed(
             distance: u(float, "meter", use_list=True),
             time: u(float, "second", use_list=True),
         ) -> u(float, "meter/second", use_list=True):
             return distance / time
+        input_args = parse_input_args(get_speed)
+        for key in ["distance", "time"]:
+            self.assertTrue(key in input_args)
+        for key in ["units", "uri", "shape"]:
+            self.assertTrue(key in input_args["distance"])
 
 
 if __name__ == "__main__":
