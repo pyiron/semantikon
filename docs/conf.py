0dabf5eef3083bf94bc6fd2f8081257a68ff4b94
samwaseda <o.waseda@mpie.de>
2024-12-05 13:33:49 +0000

    change some files

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ source_suffix = {
 master_doc = 'index'
 
 # General information about the project.
-project = u'uniton'
+project = u'semantikon'
 copyright = u'2024, Max-Planck-Institut für Nachhaltige Materialien GmbH - Computational Materials Design (CM) Department ' \
             u'All rights reserved'
 
@@ -244,7 +244,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'uniton.tex', u'uniton Documentation',
+  ('index', 'semantikon.tex', u'semantikon Documentation',
    u'Max-Planck-Institut für Nachhaltige Materialien GmbH - Computational Materials Design (CM) Department', 'manual'),
 ]
 
@@ -275,8 +275,8 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index',
-     'uniton',
-     u'uniton Documentation',
+     'semantikon',
+     u'semantikon Documentation',
      [u'Max-Planck-Institut für Nachhaltige Materialien GmbH - Computational Materials Design (CM) Department'], 1)
 ]
 
@@ -291,10 +291,10 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index',
-   'uniton',
-   u'uniton Documentation',
+   'semantikon',
+   u'semantikon Documentation',
    u'Max-Planck-Institut für Nachhaltige Materialien GmbH - Computational Materials Design (CM) Department',
-   'uniton',
+   'semantikon',
    'One line description of project.',
    'Miscellaneous'),
 ]
@@ -311,7 +311,7 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
 
-main(['-e', '-o', 'apidoc', '../uniton', '--force'])
+main(['-e', '-o', 'apidoc', '../semantikon', '--force'])
 
 curdir = os.path.dirname(os.path.abspath(__file__))
 if os.path.exists(os.path.join(curdir, 'source/notebooks')):
