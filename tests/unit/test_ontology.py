e6c5c730b5cb2afb7dff178a8f1d6bde6e839e4b
samwaseda <o.waseda@mpie.de>
2025-02-10 19:44:22 +0000

    Add first test

--- tests/unit/test_ontology.py
+++ tests/unit/test_ontology.py
@@ -1,4 +1,5 @@
 import unittest
+from rdflib import Graph, OWL, Namespace
 from semantikon.typing import u
 from semantikon.converter import (
     parse_input_args,
@@ -6,116 +7,97 @@ from semantikon.converter import (
     parse_metadata,
     get_function_dict,
 )
+from semantikon.ontology import get_knowledge_graph, PNS
 
 
-class TestUnits(unittest.TestCase):
-    def test_basic(self):
-        for use_list in [True, False]:
+EX = Namespace("http://example.org/")
 
-            @u(uri="abc")
-            def get_speed(
-                distance: u(float, units="meter", use_list=use_list),
-                time: u(float, units="second", use_list=use_list),
-            ) -> u(float, units="meter/second", label="speed", use_list=use_list):
-                return distance / time
 
-            input_args = parse_input_args(get_speed)
-            for key in ["distance", "time"]:
-                self.assertTrue(key in input_args)
-            for key in [
-                "units",
-                "uri",
-                "triples",
-                "shape",
-                "label",
-                "restrictions",
-                "dtype",
-            ]:
-                self.assertTrue(key in input_args["distance"])
-            self.assertEqual(input_args["distance"]["units"], "meter")
-            self.assertEqual(input_args["time"]["units"], "second")
-            output_args = parse_output_args(get_speed)
-            for key in [
-                "units",
-                "uri",
-                "triples",
-                "shape",
-                "label",
-                "restrictions",
-                "dtype",
-            ]:
-                self.assertTrue(key in output_args)
-            self.assertEqual(output_args["units"], "meter/second")
-            self.assertEqual(output_args["label"], "speed")
-            self.assertEqual(get_speed._semantikon_metadata["uri"], "abc")
-            self.assertRaises(TypeError, u, "abc")
-            f_dict = get_function_dict(get_speed)
-            self.assertEqual(f_dict["uri"], "abc")
-            self.assertEqual(f_dict["label"], "get_speed")
+def get_speed():
+    return {
+        "speed": {
+            "inputs": {
+                "distance": {
+                    "units": "meter",
+                    "label": None,
+                    "triples": None,
+                    "uri": None,
+                    "shape": None,
+                    "restrictions": None,
+                    "dtype": float,
+                    "value": 10.0,
+                    "connection": None,
+                },
+                "time": {
+                    "units": "second",
+                    "label": None,
+                    "triples": None,
+                    "uri": None,
+                    "shape": None,
+                    "restrictions": None,
+                    "dtype": float,
+                    "value": 2.0,
+                    "connection": None,
+                },
+            },
+            "outputs": {
+                "speed": {
+                    "units": "meter/second",
+                    "label": None,
+                    "triples": (
+                        (
+                            EX.somehowRelatedTo,
+                            "inputs.time",
+                        ),
+                        (
+                            EX.subject,
+                            EX.predicate,
+                            EX.object,
+                        ),
+                        (
+                            EX.subject,
+                            EX.predicate,
+                            None,
+                        ),
+                        (
+                            None,
+                            EX.predicate,
+                            EX.object,
+                        ),
+                    ),
+                    "uri": None,
+                    "shape": None,
+                    "restrictions": None,
+                    "dtype": float,
+                    "value": 5.0,
+                }
+            },
+            "function": {"label": "calculate_speed"},
+            "label": "speed",
+        },
+        "workflow_label": "speed",
+    }
 
-    def test_multiple_output_args(self):
-        for use_list in [True, False]:
 
-            def get_speed(
-                distance: u(float, units="meter", use_list=use_list),
-                time: u(float, units="second", use_list=use_list),
-            ) -> tuple[
-                u(float, units="meter/second", label="speed", use_list=use_list),
-                u(float, units="meter", label="distance", use_list=use_list),
-            ]:
-                return distance / time, distance
-
-            output_args = parse_output_args(get_speed)
-            self.assertIsInstance(output_args, tuple)
-            for output_arg in output_args:
-                for key in [
-                    "units",
-                    "uri",
-                    "triples",
-                    "shape",
-                    "label",
-                    "restrictions",
-                    "dtype",
-                ]:
-                    self.assertTrue(key in output_arg)
-            self.assertEqual(output_args[0]["units"], "meter/second")
-            self.assertEqual(output_args[0]["label"], "speed")
-            self.assertEqual(output_args[1]["units"], "meter")
-            self.assertEqual(output_args[1]["label"], "distance")
-
-    def test_additional_args(self):
-        def get_speed(
-            distance: u(float, units="meter", my_arg="some_info"),
-            time: u(float, units="second"),
-        ) -> u(float, units="meter/second", label="speed"):
-            return distance / time
-
-        input_args = parse_input_args(get_speed)
-        self.assertEqual(input_args["distance"]["my_arg"], "some_info")
-
-    def test_return_class(self):
-        class Output:
-            value: u(float, units="meter/second", label="speed")
-
-        def get_speed(
-            distance: u(float, units="meter"),
-            time: u(float, units="second"),
-        ) -> Output:
-            return distance / time
-
-        output_args = parse_output_args(get_speed)
-        self.assertIsInstance(output_args, dict)
-        self.assertEqual(output_args["dtype"], Output)
-
-    def test_multiple_u(self):
-        initial_type = u(float, units="meter", label="distance")
-        result = parse_metadata(initial_type)
-        self.assertEqual(result["units"], "meter")
-        self.assertEqual(result["label"], "distance")
-        final_type = u(initial_type, units="millimeter")
-        result = parse_metadata(final_type)
-        self.assertEqual(result["units"], "millimeter")
-        self.assertEqual(result["label"], "distance")
+class TestOntology(unittest.TestCase):
+    def test_units_with_sparql(self):
+        graph = get_knowledge_graph(get_speed())
+        query_txt = [
+            "PREFIX ex: <http://example.org/>",
+            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+            f"PREFIX pns: <{PNS.BASE}>",
+            "SELECT DISTINCT ?speed ?units",
+            "WHERE {",
+            "    ?output pns:hasValue ?output_tag .",
+            "    ?output_tag rdf:value ?speed .",
+            "    ?output_tag pns:hasUnits ?units .",
+            "}",
+        ]
+        query = "\n".join(query_txt)
+        results = graph.query(query)
+        self.assertEqual(len(results), 3)
+        result_list = [row[0].value for row in graph.query(query)]
+        self.assertEqual(sorted(result_list), [2.0, 5.0, 10.0])
 
 
 if __name__ == "__main__":
