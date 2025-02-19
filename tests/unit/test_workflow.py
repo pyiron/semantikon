e08948adf309c2bcb9436f7892d4f2c990d7aa62
samwaseda <o.waseda@mpie.de>
2025-02-19 20:04:39 +0000

    One more error

--- tests/unit/test_workflow.py
+++ tests/unit/test_workflow.py
@@ -7,6 +7,7 @@ class TestSnippets(unittest.TestCase):
     def test_number_to_letter(self):
         self.assertEqual(number_to_letter(0), "A")
         self.assertEqual(number_to_letter(1), "B")
+        self.assertRaises(ValueError, number_to_letter, -1)
 
 
 if __name__ == "__main__":
