6d5fb0d5d1da54256b97f285c94e88f00c0061ae
samwaseda <o.waseda@mpie.de>
2025-02-19 19:54:28 +0000

    Implement number to letter

--- semantikon/workflow.py
+++ semantikon/workflow.py
@@ -9,7 +9,7 @@ class FunctionFlowAnalyzer(ast.NodeVisitor):
         self.scope = scope
 
     def visit_Assign(self, node):
-        """Handles variable assignments (e.g., x = add(a, b) or y = func(x=val))"""
+        """Handles variable assignments including tuple unpacking."""
         if isinstance(node.value, ast.Call):  # Function call in assignment
             if isinstance(node.value.func, ast.Name):  # Direct function call
                 for ii in range(100):
@@ -19,28 +19,33 @@ class FunctionFlowAnalyzer(ast.NodeVisitor):
                 else:
                     raise AssertionError("Too many times function used")
                 if node.value.func.id not in self.scope:
-                    raise ValueError(
-                        f"Function {node.value.func.id} not defined"
-                    )
+                    raise ValueError(f"Function {node.value.func.id} not defined")
                 self.function_defs[called_func] = self.scope[node.value.func.id]
 
-                # Track function output: connect called function to the assigned variable
-                for target in node.targets:
-                    if isinstance(target, ast.Name):  # Ensure it's a variable
-                        assert target.id not in self.graph
-                        self.graph.add_edge(called_func, target.id, type="output")
+                # Track function outputs: handle single or multiple assignments
+                if len(node.targets) == 1 and isinstance(node.targets[0], ast.Tuple):
+                    # Tuple unpacking case (e.g., c, d = func(...))
+                    for index, target in enumerate(node.targets[0].elts):
+                        if isinstance(target, ast.Name):
+                            self.graph.add_edge(called_func, target.id, type="output", output_index=index)
+                else:
+                    # Single assignment case (e.g., x = func(...))
+                    for target in node.targets:
+                        if isinstance(target, ast.Name):  # Ensure it's a variable
+                            self.graph.add_edge(called_func, target.id, type="output")
 
                 # Track positional arguments
                 for index, arg in enumerate(node.value.args):
                     if isinstance(arg, ast.Name):  # If argument is a variable
-                        self.graph.add_edge(arg.id, called_func, type="input", arg_index=index)
+                        self.graph.add_edge(arg.id, called_func, type="input", input_index=index)
 
                 # Track keyword arguments
                 for kw in node.value.keywords:
                     if isinstance(kw.value, ast.Name):  # If the value is a variable
-                        self.graph.add_edge(kw.value.id, called_func, type="input", arg_name=kw.arg)
+                        self.graph.add_edge(kw.value.id, called_func, type="input", input_name=kw.arg)
 
         self.generic_visit(node)
+
 
 
 def analyze_function(func):
@@ -52,3 +57,9 @@ def analyze_function(func):
     analyzer.visit(tree)
     return analyzer
 
+
+def number_to_letter(n):
+    if 0 <= n <= 25:
+        return chr(n + ord('A'))
+    else:
+        raise ValueError("Number out of range")
