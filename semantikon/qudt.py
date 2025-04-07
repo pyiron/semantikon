187c5efba6120f9006b9f0a8302b7a05e4faa0a1
samwaseda <o.waseda@mpie.de>
2025-04-07 07:33:20 +0000

    Add possibility of download latest data

--- semantikon/qudt.py
+++ semantikon/qudt.py
@@ -2,6 +2,7 @@ import re
 import os
 from rdflib import Graph, RDFS
 from pint import UnitRegistry
+import requests
 
 
 def contains_special_char(s):
@@ -9,9 +10,33 @@ def contains_special_char(s):
     return bool(re.search(r'[^a-zA-Z0-9_./ ]', s))
 
 
+def download_data(version="3.1.0", store_data=False):
+    data = requests.get(f"https://qudt.org/{version}/vocab/unit").text
+    graph = Graph()
+    graph.parse(data=data, format="ttl")
+    graph_with_only_label = Graph()
+    for s, p, o in graph:
+        if p == RDFS.label:
+            graph_with_only_label.add((s, p, o))
+    if store_data:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        location = os.path.join(script_dir, 'data', 'qudt.ttl')
+        graph_with_only_label.serialize(destination=location, format="ttl")
+    return graph_with_only_label
+
+
 class UnitsDict:
-    def __init__(self, graph=None, location=None):
-        if graph is None:
+    def __init__(
+        self,
+        graph=None,
+        location=None,
+        force_download=False,
+        version=None,
+        store_data=False,
+    ):
+        if force_download:
+            graph = download_data(version=version, store_data=store_data)
+        elif graph is None:
             graph = get_graph(location)
         self._units_dict = get_units_dict(graph)
         self._ureg = UnitRegistry()
