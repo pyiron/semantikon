96646674fb1f2ddb96c93aff11c217c27114b2d7
samwaseda <o.waseda@mpie.de>
2025-04-28 06:45:02 +0000

    Add doc

--- semantikon/visualize.py
+++ semantikon/visualize.py
@@ -5,6 +5,15 @@ from rdflib.tools.rdf2dot import rdf2dot
 
 
 def visualize(graph):
+    """
+    Visualize an RDF graph using pydotplus and return a PNG image.
+
+    Args:
+        graph (rdflib.Graph): The RDF graph to visualize.
+
+    Returns:
+        IPython.display.Image: The PNG image of the RDF graph.
+    """
     stream = io.StringIO()
     rdf2dot(graph, stream, opts={"rankdir=LR"})
     dg = pydotplus.graph_from_dot_data(stream.getvalue())
