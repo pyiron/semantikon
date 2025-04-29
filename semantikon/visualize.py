c422c38503e00f5c4c85dc69e80e5d4211097828
samwaseda <o.waseda@mpie.de>
2025-04-29 08:47:07 +0000

    Rewrite functions

--- semantikon/visualize.py
+++ semantikon/visualize.py
@@ -1,21 +1,60 @@
-import io
-import pydotplus
-from IPython.display import Image
-from rdflib.tools.rdf2dot import rdf2dot
+from rdflib import RDF, URIRef, Literal, BNode, Graph
+from graphviz import Digraph
+
+
+def short_label(graph, node):
+    """Use graph's prefixes to shorten URIs nicely."""
+    if isinstance(node, URIRef):
+        try:
+            return graph.qname(node)
+        except Exception:
+            return str(node)
+    elif isinstance(node, BNode):
+        return f"_:{str(node)}"
+    elif isinstance(node, Literal):
+        return f'"{str(node)}"'
+    else:
+        return str(node)
+
+
+def get_value(graph, label, **kwargs):
+    if ":" in label:
+        return {"prefix": dict(graph.namespaces())[label.split(":")[0]]} | kwargs
+    else:
+        return kwargs
+
+
+def get_data(graph):
+    data_dict = {}
+    edge_list = []
+    for subj, value in graph.subject_objects(RDF.value):
+        label = short_label(graph, subj)
+        data_dict[label] = get_value(graph, label, value=str(value.toPython()))
+    
+    for subj, pred, obj in graph:
+        if pred == RDF.value:
+            continue
+        edges = []
+        for tag in [subj, obj]:
+            label = short_label(graph, tag)
+            if label not in data_dict:
+                data_dict[label] = get_value(graph, label)
+            edges.append(label.replace(":", "_"))
+        edges.append(short_label(graph, pred))
+        edge_list.append(edges)
+    return data_dict, edge_list
 
 
 def visualize(graph):
-    """
-    Visualize an RDF graph using pydotplus and return a PNG image.
+    dot = Digraph(comment="RDF Graph", format='png')
+    dot.attr('node', shape='record')
+    data_dict, edge_list = get_data(graph)
+    for key, value in data_dict.items():
+        if len(value) == 0:
+            dot.node(key.replace(":", "_"), key)
+        else:
+            dot.node(key.replace(":", "_"), "{" + " | ".join([key] + [f"{kk}: {vv}" for kk, vv in value.items()]) + "}")
+    for edges in edge_list:
+        dot.edge(edges[0], edges[1], label=edges[2])
 
-    Args:
-        graph (rdflib.Graph): The RDF graph to visualize.
-
-    Returns:
-        IPython.display.Image: The PNG image of the RDF graph.
-    """
-    stream = io.StringIO()
-    rdf2dot(graph, stream, opts={"rankdir=LR"})
-    dg = pydotplus.graph_from_dot_data(stream.getvalue())
-    png = dg.create_png()
-    return Image(png)
+    return dot
