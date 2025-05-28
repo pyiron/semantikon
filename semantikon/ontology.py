40be0b61e40f68a40e3c3944c23576f71e5dcb6c
samwaseda <o.waseda@mpie.de>
2025-05-28 20:43:12 +0000

    Revert "run black"
    This reverts commit 1e4a8710395ffc2c2f16f63bb76dc5e0532925a9.

--- semantikon/ontology.py
+++ semantikon/ontology.py
@@ -28,9 +28,7 @@ class NS:
 
 ud = UnitsDict()
 
-_triple_type: TypeAlias = list[
-    tuple[IdentifiedNode | str | None, URIRef, IdentifiedNode | str | None]
-]
+_triple_type: TypeAlias = list[tuple[IdentifiedNode | str | None, URIRef, IdentifiedNode | str | None]]
 
 
 def _translate_has_value(
