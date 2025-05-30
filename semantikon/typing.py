9853e8ed4dae3e634d8f4a856d679b374c8ed73d
liamhuber <liamhuber@greyhavensolutions.com>
2025-05-30 11:39:47 -0700

    Use dataclasses for metadata
    This is a purely back-end change; other than the new "not an entry" default of `MISSING` instead of `None`, this is not even visible to the user. No tests had to be adjusted; behaviour is preserved.
    
    Buuuut, only because we manually include all the **kwargs used in the tests when annotating types -- this is not actually a robust and universal solution yet.
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- semantikon/typing.py
+++ semantikon/typing.py
@@ -1,6 +1,16 @@
 from typing import Annotated, Any, Callable, get_origin
 
 from semantikon.converter import FunctionWithMetadata, parse_metadata
+from semantikon.dataclasses import (
+    RestrictionLike,
+    ShapeType,
+    TriplesLike,
+    CoreMetadata,
+    TypeMetadata,
+    Missing,
+    MISSING,
+)
+
 
 __author__ = "Sam Waseda"
 __copyright__ = (
@@ -21,89 +31,75 @@ def _is_annotated(type_):
 def _type_metadata(
     type_,
     /,
-    units: str | None = None,
-    label: str | None = None,
-    triples: tuple[tuple[str, str, str], ...] | tuple[str, str, str] | None = None,
-    uri: str | None = None,
-    shape: tuple[int] | None = None,
-    restrictions: tuple[tuple[str, str]] | None = None,
+    uri: str | Missing = MISSING,
+    triples: TriplesLike | Missing = MISSING,
+    restrictions: RestrictionLike | Missing = MISSING,
+    label: str | Missing = MISSING,
+    units: str | Missing = MISSING,
+    shape: ShapeType | Missing = MISSING,
     **kwargs,
 ) -> Any:
+    presently_requested_metadata = TypeMetadata(
+        uri=uri,
+        triples=triples,
+        restrictions=restrictions,
+        label=label,
+        units=units,
+        shape=shape,
+    )
     if _is_annotated(type_):
         kwargs.update(parse_metadata(type_))
         type_ = type_.__origin__
-    kwargs.update(
-        _kwargs_to_dict(
-            units=units,
-            label=label,
-            triples=triples,
-            uri=uri,
-            shape=shape,
-            restrictions=restrictions,
-        )
-    )
+    kwargs.update(**presently_requested_metadata.to_dictionary())
     if len(kwargs) == 0:
         raise TypeError("No metadata provided.")
-    items = tuple([x for k, v in kwargs.items() for x in [k, v]])
+    metadata = TypeMetadata(**kwargs)
+    items = tuple([x for k, v in metadata.to_dictionary().items() for x in [k, v]])
     return Annotated[type_, items]
 
 
 def _function_metadata(
-    triples: tuple[tuple[str, str, str], ...] | tuple[str, str, str] | None = None,
-    uri: str | None = None,
-    restrictions: tuple[tuple[str, str]] | None = None,
-    **kwargs,
+    uri: str | Missing = MISSING,
+    triples: TriplesLike | Missing = MISSING,
+    restrictions: RestrictionLike | Missing = MISSING,
 ):
-    data: dict[str, object] = {
-        k: v
-        for k, v in {
-            "triples": triples,
-            "uri": uri,
-            "restrictions": restrictions,
-        }.items()
-        if v is not None
-    }
-    data.update(kwargs)
-    for key, value in kwargs.items():
-        if value is None:
-            data.pop(key)
 
     def decorator(func: Callable):
         if not callable(func):
             raise TypeError(f"Expected a callable, got {type(func)}")
-        return FunctionWithMetadata(func, data)
+        return FunctionWithMetadata(
+            func,
+            CoreMetadata(
+                triples=triples, uri=uri, restrictions=restrictions
+            ).to_dictionary(),
+        )
 
     return decorator
-
-
-def _kwargs_to_dict(**kwargs):
-    return {k: v for k, v in kwargs.items() if v is not None}
 
 
 def u(
     type_or_func=None,
     /,
-    units: str | None = None,
-    label: str | None = None,
-    triples: tuple[tuple[str, str, str], ...] | tuple[str, str, str] | None = None,
-    uri: str | None = None,
-    shape: tuple[int] | None = None,
-    restrictions: tuple[tuple[str, str]] | None = None,
+    uri: str | Missing = MISSING,
+    triples: TriplesLike | Missing = MISSING,
+    restrictions: RestrictionLike | Missing = MISSING,
+    label: str | Missing = MISSING,
+    units: str | Missing = MISSING,
+    shape: ShapeType | Missing = MISSING,
     **kwargs,
-) -> Any:
-    kwargs.update(
-        _kwargs_to_dict(
-            units=units,
-            label=label,
-            triples=triples,
-            uri=uri,
-            shape=shape,
-            restrictions=restrictions,
-        )
-    )
+) -> Callable[[Callable], FunctionWithMetadata] | Annotated:
     if isinstance(type_or_func, type) or get_origin(type_or_func) is not None:
-        return _type_metadata(type_or_func, **kwargs)
+        return _type_metadata(
+            type_or_func,
+            uri=uri,
+            triples=triples,
+            restrictions=restrictions,
+            label=label,
+            units=units,
+            shape=shape,
+            **kwargs,
+        )
     elif type_or_func is None:
-        return _function_metadata(**kwargs)
+        return _function_metadata(uri=uri, triples=triples, restrictions=restrictions)
     else:
         raise TypeError(f"Unsupported type: {type(type_or_func)}")
