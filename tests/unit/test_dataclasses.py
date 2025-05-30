0724dfe6b62a26557acc3838bad2ffe77073d225
liamhuber <liamhuber@greyhavensolutions.com>
2025-05-30 09:48:26 -0700

    Test optionality
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- tests/unit/test_dataclasses.py
+++ tests/unit/test_dataclasses.py
@@ -7,6 +7,7 @@ import semantikon.dataclasses as sdc
 @dataclasses.dataclass
 class ConcreteDC(sdc._VariadicDataclass):
     complex_field: set[str]
+    optional_field: int | sdc.Missing = sdc.missing()
 
 
 class TestDataclasses(unittest.TestCase):
@@ -24,4 +25,18 @@ class TestDataclasses(unittest.TestCase):
             self.dc.complex_field,
             self.dc.to_dictionary()["complex_field"],
             msg="The dictionary representation should return the same underlying data",
+        )
+
+    def test_missing(self):
+        self.assertIs(
+            self.dc.optional_field,
+            sdc.MISSING,
+            msg="Dataclass field should hold full data object, even missing objects"
+        )
+        self.assertNotIn(
+            "optional_field",
+            self.dc.to_dictionary(),
+            msg="Sending the dataclass to a dictionary should purge missing entries --"
+                "this is why we call it a variadic dataclass, because some of its "
+                "fields are optional!"
         )
