d0de097e3a833e24ec712b65108b4d464cc47278
liamhuber <liamhuber@greyhavensolutions.com>
2025-06-02 10:43:54 -0700

    Use dataclasses for metadata
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- semantikon/dataclasses.py
+++ semantikon/dataclasses.py
@@ -2,7 +2,7 @@ import abc
 import dataclasses
 import functools
 from collections.abc import Iterable
-from typing import Any, Iterator
+from typing import Any, Iterator, TypeAlias
 
 
 class Missing:
@@ -33,3 +33,31 @@ class _VariadicDataclass(_HasToDictionary):
 
     def __iter__(self) -> Iterator[tuple[str, Any]]:
         yield from ((f.name, getattr(self, f.name)) for f in dataclasses.fields(self))
+
+
+TripleType: TypeAlias = tuple[str, str, str]
+TriplesLike: TypeAlias = tuple[TripleType, ...] | TripleType
+RestrictionType: TypeAlias = tuple[str, str]
+RestrictionLike: TypeAlias = tuple[RestrictionType, ...] | RestrictionType
+ShapeType: TypeAlias = tuple[int, ...]
+
+
+@dataclasses.dataclass(slots=True)
+class CoreMetadata(_VariadicDataclass):
+    uri: str | Missing = missing()
+    triples: TriplesLike | Missing = missing()
+    restrictions: RestrictionLike | Missing = missing()
+
+
+@dataclasses.dataclass(slots=True)
+class TypeMetadata(CoreMetadata):
+    label: str | Missing = missing()
+    units: str | Missing = missing()
+    shape: ShapeType | Missing = missing()
+
+    # Stuff that gets passed to _type_metadata during the tests
+    associate_to_sample: bool | Missing = missing()
+    cancel: tuple[Any, Any] | Missing = missing()
+    my_arg: str | Missing = missing()
+    use_list: bool | Missing = missing()
+    # How should we handle this?
