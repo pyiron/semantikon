2db413bd7659f211ebcb14c2532b318f8603f61d
liamhuber <liamhuber@greyhavensolutions.com>
2025-06-02 10:39:23 -0700

    Introduce dataclass tools
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

---
+++ semantikon/dataclasses.py
@@ -0,0 +1,35 @@
+import abc
+import dataclasses
+import functools
+from collections.abc import Iterable
+from typing import Any, Iterator
+
+
+class Missing:
+    def __repr__(self):
+        return "<MISSING>"
+
+
+MISSING = Missing()
+missing = functools.partial(dataclasses.field, default=MISSING)
+
+
+class _HasToDictionary(Iterable[tuple[str, Any]], abc.ABC):
+    @abc.abstractmethod
+    def __iter__(self) -> Iterator[tuple[str, Any]]: ...
+
+    def to_dictionary(self) -> dict[str, Any]:
+        d = {}
+        for k, v in self:
+            if isinstance(v, _HasToDictionary):
+                d[k] = v.to_dictionary()
+            elif v is not MISSING:
+                d[k] = v
+        return d
+
+
+@dataclasses.dataclass(slots=True)
+class _VariadicDataclass(_HasToDictionary):
+
+    def __iter__(self) -> Iterator[tuple[str, Any]]:
+        yield from ((f.name, getattr(self, f.name)) for f in dataclasses.fields(self))
