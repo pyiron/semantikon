c568aa406c6de0676c7ececf3575690f8163dcbe
Sam Waseda <waseda@mpie.de>
2025-06-17 02:52:29 +0200

    Raise error if the number of outputs does not correspond to the number of variables

--- semantikon/workflow.py
+++ semantikon/workflow.py
@@ -477,6 +477,12 @@ def _get_nodes(
     for label, function in data.items():
         func = function["function"]
         if hasattr(func, "_semantikon_workflow"):
+            assert label in output_counts
+            if output_counts[label] != len(func._semantikon_workflow["outputs"]):
+                raise ValueError(
+                    f"{label} has {len(func._semantikon_workflow['outputs'])} outputs, "
+                    f"but {output_counts[label]} expected"
+                )
             data_dict = func._semantikon_workflow.copy()
             result[label] = data_dict
             result[label]["label"] = label
