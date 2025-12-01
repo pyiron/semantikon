5ace264894da3b41c87445922b7d37fed5d28439
Sam Waseda <waseda@mpie.de>
2025-12-01 16:51:04 +0100

    New line that had been erased by mistake

--- semantikon/converter.py
+++ semantikon/converter.py
@@ -1,4 +1,5 @@
-# coding: utf-8 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 import ast
