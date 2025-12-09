4269e9a97613bdecda4f13389cb571a43d44d9e2
Sam Waseda <waseda@mpie.de>
2025-12-09 18:02:50 +0100

    ruff-sort-imports

--- tests/unit/test_ontology.py
+++ tests/unit/test_ontology.py
@@ -9,7 +9,6 @@ from semantikon import ontology as onto
 from semantikon.metadata import u
 from semantikon.workflow import workflow
 
-
 EX: Namespace = Namespace("http://example.org/")
 PMD: Namespace = Namespace("https://w3id.org/pmd/co/PMD_")
 
