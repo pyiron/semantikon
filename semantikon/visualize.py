ffcfb48a82d7a67888efbebad313b8b4d6a51e7a
Sam Waseda <waseda@mpie.de>
2026-01-14 11:16:42 +0100

    refactor more components

--- semantikon/visualize.py
+++ semantikon/visualize.py
@@ -21,14 +21,8 @@ def _get_triples(graph: Graph):
         for subj, pred, obj in graph.query(query.replace("R_TYPE", rest)):
             yield subj, pred, obj, style
 
-def visualize_recipe(graph, engine="dot"):
-    cl_lst = []
-    color_dict = {
-        "bfo:0000015": "red",
-        "obi:0001933": "yellow",
-        "pmdco:0000066": "green",
-        "pmdco:0000067": "blue",
-    }
+
+def _rename_predicate(pred: str) -> str:
     edge_dict = {
         "bfo:0000051": "bfo:has_part",
         "bfo:0000063": "bfo:precedes",
@@ -38,24 +32,45 @@ def visualize_recipe(graph, engine="dot"):
         "obi:0001927": "obi:specified_values_of",
         "ro:0000057": "pmd:output_assignment",
     }
+    return edge_dict.get(*2 * [pred])
+
+
+def _get_node_color(comp: str, graph: Graph) -> str:
+    color_dict = {
+        "bfo:0000015": "red",
+        "obi:0001933": "yellow",
+        "pmdco:0000066": "green",
+        "pmdco:0000067": "blue",
+    }
+    parent_classes = [
+        item
+        for item in graph.objects(comp, RDFS.subClassOf)
+        if isinstance(item, URIRef)
+    ]
+    for cl in parent_classes:
+        if graph.qname(cl) in color_dict:
+            return color_dict[graph.qname(cl)]
+    return "black"
+
+
+def _rdflib_to_nx(graph: Graph) -> nx.DiGraph:
     G = nx.DiGraph()
     for subj, pred, obj, style in _get_triples(graph):
         for part in [subj, obj]:
             if part in G.nodes:
                 continue
-            color = "black"
-            parent_classes = [
-                item
-                for item in graph.objects(part, RDFS.subClassOf)
-                if isinstance(item, URIRef)
-            ]
-            for cl in parent_classes:
-                if graph.qname(cl) in color_dict:
-                    color = color_dict[graph.qname(cl)]
-                    break
-            G.add_node(graph.qname(part), color=color)
-        pred = edge_dict.get(* 2 * [graph.qname(pred)])
-        G.add_edge(graph.qname(subj), graph.qname(obj), label=pred, style=style)
+            G.add_node(graph.qname(part), color=_get_node_color(part, graph))
+        G.add_edge(
+            graph.qname(subj),
+            graph.qname(obj),
+            label=_rename_predicate(graph.qname(pred)),
+            style=style,
+        )
+    return G
+
+
+def visualize_recipe(graph: Graph) -> Digraph:
+    G = _rdflib_to_nx(graph)
     dot = Digraph()
     for node, data in G.nodes.data():
         dot.node(sha256(node.encode()).hexdigest(), node, **data)
