5a6c7c25ed0c6818290ecb0c1ff697354a96e1fd
liamhuber <liamhuber@greyhavensolutions.com>
2026-05-06 12:05:27 -0700

    Ruff sort imports
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- semantikon/api.py
+++ semantikon/api.py
@@ -1,6 +1,6 @@
 from semantikon.analysis import query_io_completer, request_values
-from semantikon.datastructure import FunctionMetadata, TypeMetadata
 from semantikon.converter import parse_metadata, semantikon_dataclass
+from semantikon.datastructure import FunctionMetadata, TypeMetadata
 from semantikon.metadata import SemantikonURI, meta, u
 from semantikon.ontology import get_knowledge_graph, validate_values
 from semantikon.visualize import visualize_recipe
