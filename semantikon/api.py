d302f44f4c4d4bdcbd0ac6f0b922310d37b27d69
liamhuber <liamhuber@greyhavensolutions.com>
2026-05-06 07:28:01 -0700

    Add `parse_metadata` and metadata dataclasses to api
    Per https://github.com/pyiron/semantikon/issues/448.
    
    I took the liberty of alphabetizing "semantikon_dataclass" in the __all__ list while I'm here.
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- semantikon/api.py
+++ semantikon/api.py
@@ -1,16 +1,20 @@
 from semantikon.analysis import query_io_completer, request_values
-from semantikon.converter import semantikon_dataclass
+from semantikon.datastructure import FunctionMetadata, TypeMetadata
+from semantikon.converter import parse_metadata, semantikon_dataclass
 from semantikon.metadata import SemantikonURI, meta, u
 from semantikon.ontology import get_knowledge_graph, validate_values
 from semantikon.visualize import visualize_recipe
 
 __all__ = [
+    "FunctionMetadata",
     "SemantikonURI",
-    "semantikon_dataclass",
+    "TypeMetadata",
     "get_knowledge_graph",
     "meta",
+    "parse_metadata",
     "query_io_completer",
     "request_values",
+    "semantikon_dataclass",
     "u",
     "validate_values",
     "visualize_recipe",
