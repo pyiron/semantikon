b4bee00a9a6f2d4bd32758f91671e6f03382b573
liamhuber <liamhuber@greyhavensolutions.com>
2026-05-31 21:56:08 -0700

    Expose converter in API
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- semantikon/api.py
+++ semantikon/api.py
@@ -1,6 +1,7 @@
 from semantikon.analysis import query_io_completer, request_values
 from semantikon.converter import parse_metadata, semantikon_dataclass
 from semantikon.datastructure import FunctionMetadata, TypeMetadata
+from semantikon.flowrep_dict import nodedata2dict
 from semantikon.metadata import SemantikonURI, meta, u
 from semantikon.ontology import get_knowledge_graph, validate_values
 from semantikon.visualize import visualize_recipe
@@ -10,6 +11,7 @@ __all__ = [
     "SemantikonURI",
     "TypeMetadata",
     "get_knowledge_graph",
+    "nodedata2dict",
     "meta",
     "parse_metadata",
     "query_io_completer",
