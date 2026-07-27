715eb86c2182be521764b962e933a1976db36736
Sam Waseda <waseda@mpie.de>
2026-07-27 14:46:32 +0200

    style: remove coding

--- semantikon/converter.py
+++ semantikon/converter.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
